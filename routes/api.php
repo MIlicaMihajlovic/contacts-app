5fbcbc62bf5925d1a9e49185be10bda08a588189
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-12-13 21:18:44 +0100

    Add middleware on resource route

--- routes/api.php
+++ routes/api.php
@@ -27,12 +27,18 @@ Route::group([
     Route::post('login', 'AuthController@login');
 });
 
+Route::middleware('auth:api')->group(function() {
+    Route::resource('contacts', ContactsController::class)
+    ->except(['create', 'edit']);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 //Route::get('contacts', 'ContactsController@index');
 
-Route::resource('contacts', ContactsController::class)
-    ->except(['create', 'edit']);
+// Route::resource('contacts', ContactsController::class)
+//     ->except(['create', 'edit']);
+
     //da ne bi vracalo gresku 500 onda stavimo da ne ide na te rute
